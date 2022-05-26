a6752aa62ea98bbf750df12c4d0f78d4c14be317
João Pedro Niz de Assis <99514915+Zileey@users.noreply.github.com>
2022-05-26 20:50:25 -0300

    Aula 14
    Exercicio 1

---
+++ src/br/inatel/cdg/exercicio1/Main.java
@@ -0,0 +1,36 @@
+package br.inatel.cdg.exercicio1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Path arquivo = Paths.get("professores_disciplina.txt");
+
+        try {
+            List<String> conteudo = Files.readAllLines(arquivo);
+            Map<String,String> professores_disciplinas = new HashMap<>();
+
+            conteudo.forEach((linha) -> {
+                String[] linhaQuebrada = linha.split("=");
+                professores_disciplinas.put(linhaQuebrada[0],linhaQuebrada[1].strip());
+            });
+
+            professores_disciplinas.forEach((professor, disciplina) -> {
+                System.out.println("Professor: " + professor);
+                System.out.println("Disciplina: " + disciplina);
+                System.out.println("");
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
